df3f1f7a211af9b76b3fa32717c49c2fc5509d23
Michael Gemar <michaelgemar@gmail.com>
2016-07-11 17:49:02 -0700

    accounts working

--- client/main.js
+++ client/main.js
@@ -1,11 +1,22 @@
 Template.operator.helpers({
 	messages() {
-		return Messages.find();
+		if (Router.current().params.id)
+			return Messages.find({user: Router.current().params.id});
+	},
+	users() {
+		return Meteor.users.find().map(function (doc) {
+			return { email: doc.emails[0].address, id: doc._id, active: doc._id == Router.current().params.id};
+		})
 	}
 });
 
 Template.registerHelper('fromNow', function (date) {
 	return moment(date).fromNow()
+});
+
+Template.registerHelper('name', function () {
+	if (Meteor.user())
+		return Meteor.user().emails[0].address;
 });
 
 Template.operator.events({
@@ -18,10 +29,13 @@ Template.operator.events({
 		Messages.insert({
 			text: e.target.message.value,
 			created: new Date(),
-			user: null,
+			user: Router.current().params.id,
 			prudence: true,
 		});
 		e.target.message.value = null;
+		setTimeout(function () {
+			$('.chat-history').scrollTop($('.chat-history').height());
+		}, 100);
 	}
 });
 
@@ -30,8 +44,47 @@ $('#myTabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
-})
+	setTimeout(function () {
+		$('.chat-history').scrollTop($('.chat-history').height());
+	}, 100);
+});
 
 Router.configure({layoutTemplate: 'layout'});
-Router.route('/', { name: 'operator'})
-Router.route('/user', { name: 'user'})
+Router.route('/', { name: 'user'})
+Router.route('/prudence', { template: 'operator'})
+Router.route('/prudence/:id', { template: 'operator'})
+
+Template.user.helpers({
+	messages() {
+		return Messages.find({user: Meteor.userId()});
+	},
+	name() {
+		return Meteor.user().email;
+	}
+});
+
+Template.user.onRendered(function () {
+	setTimeout(function () {
+		$('.chat-history').scrollTop($('.chat-history').height());
+	}, 100);
+});
+
+Template.user.events({
+	'keypress textarea'(e, i) {
+		if (e.keyCode == 13)
+			$('#messageForm').submit();
+	},
+	'submit form'(e, i) {
+		e.preventDefault();
+		Messages.insert({
+			text: e.target.message.value,
+			created: new Date(),
+			user: Meteor.userId()
+		});
+		e.target.message.value = null;
+		setTimeout(function () {
+			$('.chat-history').scrollTop($('.chat-history').height());
+		}, 100);
+	}
+
+});
